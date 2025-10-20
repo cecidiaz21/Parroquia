0222db954591a7eee4a9fcb7417136690cb71d84
Cecilia <126418068+cecidiaz21@users.noreply.github.com>
2025-10-19 23:17:49 -0300

    Primer commit
    Faltan mas archivos

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'navy': '#0A2463',
+        'sky': '#3E92CC',
+        'white': '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
